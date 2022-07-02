ffa196ed9f1813b7567e27d9362a0ced1bbcd33f
Seyed Ali Seyed Hossein <seyed.a.farzaneh@hotmail.com>
2022-07-03 02:13:43 +0300

    Uploaded the HW1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿int health = 200;
+string Name = "Hero";
+
+int damaged = health - 20;
+health = damaged;
+Console.WriteLine("Damaged Health = " + health);
+int boost = health + 10;
+Console.WriteLine("Boosted Health = " + boost);
+if (boost>damaged)
+
+{
+    Console.WriteLine(boost + " is greater than " + damaged);
+
+}
+
+else
+
+{
+    Console.WriteLine(damaged + " is greater than " + boost);
+
+}
